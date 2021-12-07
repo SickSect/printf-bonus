a73df4dd3f16fe51529c181b5719be4b02b552e3
SickSect <sleepyboy906sb@gmail.com>
2021-12-07 19:58:35 +0300

    test digit later

--- ft_digit.c
+++ ft_digit.c
@@ -17,36 +17,87 @@ int ft_numlen(int n)
     }
     return(len + 1);
 }
-void ft_putsign(flg_stc *flg, int n, char *sign)
+void ft_filler(char fill, int counter, int *bytes)
 {
-    if (n < 0)
-        *sign = '-';
-    else
-    {
-        if (flg->flg_spc == 1)
-            *sign = ' ';
-        if (flg->flg_pls == 1)
-            *sign = '+';
-    }
-}
+  int i;
 
+  i = 0;
+  while (i < counter)
+  {
+    ft_putchar(fill, bytes);
+    i++;
+  }
+}
+int ft_if_sign(flg_stc *flg, int n)
+{
+  if(flg->flg_pls == 1 && n > 0)
+    return(1);
+  else if(flg->flg_spc == 1 && n > 0)
+    return (1);
+  else if(n < 0)
+    return(1);
+  return (0);
+}
+void ft_output_sign(flg_stc *flg, int n, int *bytes)
+{
+  if(flg->flg_pls == 1 && n > 0)
+    ft_putchar('+', bytes);
+  else if(flg->flg_spc == 1 && n > 0)
+    ft_putchar(' ', bytes);
+  if(n < 0)
+    ft_putchar('-', bytes);
+}
 void ft_digit(int n, int *bytes,flg_stc *flg)
 {
-    char sign;
-    ft_putsign(flg, n, &sign);
-    if(flg->press == -1)
+    if(flg->flg_mns == 1)
     {
-        if(flg->flg_mns == 1)
-        {
-
-        }
+      /*
+      if(flg->width > flg->press && flg->press > 0)
+      {
+        ft_output_sign(flg, n, bytes);
+        ft_filler('0', flg->press - ft_numlen(n), bytes);
+        if (n > 0)
+          ft_putnbr(n, bytes);
+        else
+          ft_putnbr(n * -1, bytes);
+        ft_filler(' ',flg->width - flg->press - ft_if_sign(flg, n), bytes);
+      }
+      */
+        ft_output_sign(flg, n, bytes);
+        ft_filler('0', flg->press - ft_numlen(n), bytes);
+        if (n > 0)
+          ft_putnbr(n, bytes);
+        else
+          ft_putnbr(n * -1, bytes);
+        if(flg->width > flg->press && flg->press > 0)
+          ft_filler(' ',flg->width - flg->press - ft_if_sign(flg, n), bytes);
+        else if (flg->press == -1)
+          ft_filler(' ', flg->width - ft_numlen(n) - ft_if_sign(flg, n), bytes);
     }
-    else if(flg->press >= 0)
+    else if (flg->flg_zro == 1)
     {
-
+      ft_output_sign(flg, n, bytes);
+      ft_filler('0', flg->width - ft_numlen(n) - ft_if_sign(flg, n), bytes);
+      if (n > 0)
+        ft_putnbr(n, bytes);
+      else
+        ft_putnbr(n * -1, bytes);
+    }
+    else
+    {
+      if(flg->width > flg->press && flg->press > 0)
+        ft_filler(' ',flg->width - flg->press - ft_if_sign(flg, n), bytes);
+      else if (flg->press == -1 || flg->press == 0)
+        ft_filler(' ', flg->width - ft_numlen(n) - ft_if_sign(flg, n), bytes);
+      ft_output_sign(flg, n, bytes);
+      if(flg->press > 0)
+        ft_filler('0', flg->press - ft_numlen(n), bytes);
+      if (n > 0)
+        ft_putnbr(n, bytes);
+      else
+        ft_putnbr(n * -1, bytes);
     }
     (void)n;
     (void)bytes;
     (void)flg;
-    (void)sign;
 }
